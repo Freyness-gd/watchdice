bc95601518766995e52b21b5b4d779d53cc91069
freyness <gabriel@dinu.tech>
2024-08-18 11:01:29 +0200

    :label: Change name of rating entity attribute

--- lib/layers/domain/entity/rating.dart
+++ lib/layers/domain/entity/rating.dart
@@ -3,16 +3,16 @@ import 'package:equatable/equatable.dart';
 class Rating with EquatableMixin {
   Rating({
     this.source,
-    this.score,
+    this.value,
   });
 
   final String? source;
-  final String? score;
+  final String? value;
 
   @override
   // TODO: implement props
   List<Object?> get props => [
         source,
-        score,
+        value,
       ];
 }
